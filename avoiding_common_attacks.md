ec5cdf6df135870d2e2e60dbcd556201052e0efc
rafael <reaction.hashs@gmail.com>
2019-01-28 03:03:55 -0200

    Commented Vote.sol and updated avoiding_common_attacks.md

--- avoiding_common_attacks.md
+++ avoiding_common_attacks.md
@@ -1,1 +1,22 @@
-# Avoiding common atacks
+# Avoiding common attacks
+
+  - [Main contract](#main-contract)
+  - [Ownership](#ownership)
+  - [Freeze](#freeze)
+  - [Kill function](#kill-function)
+
+## Main contract 
+
+Constructor of **Vote.sol** initializes 'owner' with her/his address. **Stop.sol** is imported into the main contract. 
+
+## Ownership 
+
+**Ownable.sol** makes sure there is only one account that is the owner and can do administrative tasks on contracts. This contract is imported into **Stop.sol**.
+
+## Freeze
+
+Owner can freeze the contract in case of errors. This is made through **Stop.sol**. 
+
+## Kill function
+
+Owner of the contract can terminate it by using kill function. Also on **Stop.sol**.
--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "./Stop.sol";
 
 contract Vote is Stop {
-
+    // Creates the structure of a 'candidate' in our election.
     struct Candidate {
         uint id;
         string name;
@@ -12,23 +12,33 @@ contract Vote is Stop {
 
     }
 
+    // Creates the structure of a 'party' in our election (for further implementation).
     struct Party {
         string acronym;
         string partyName;
         uint number;
     }
 
+    // Owner's adress.
     address public owner;
+    // Mapping to store 'voter' accounts, so we can later avoid that they vote more than one time.
     mapping(address => bool) public voters;
+    // Mapping to store 'candidates'.
     mapping(uint => Candidate) public candidates;
+    // Mapping to store 'parties'.
     mapping(uint => Party) public parties;
-    
+    // Candidates count.
     uint public candidatesCount;
-
+    // Event to be triggered by vote() function.
     event votedEvent (
         uint indexed _candidateId
     );
 
+    /*
+    Constructor function initialized with the owner's address.
+    Calls addCandidate() for each one of the 13 candidates running for presidency.
+    Each candidate has a 'name' and a 'party'.
+    */
     constructor() public {
         owner = msg.sender;
 
@@ -47,11 +57,19 @@ contract Vote is Stop {
         addCandidate("Vera", "PSTU");
     }
 
+    // Adds a candidate to the race, asking for a 'name' and a 'party' for each one. Increments candidatesCount.
     function addCandidate (string memory _name, string memory _party) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0, _party);
     }
 
+    /*
+    Vote function, asking for a candidateId. 
+    It has an emergencySwitch, imported from Stop.sol.
+    Stores new voters and makes sure the address has not voted before.
+    Requires vote in a valid candidate (candidateId > 0 and <= candidatesCount).
+    Increments voteCount once candidate has been voted and trigger 'voted' event.
+    */
     function vote (uint _candidateId) public emergencySwitch {
         require(!voters[msg.sender]);
         require(_candidateId > 0 && _candidateId <= candidatesCount);
